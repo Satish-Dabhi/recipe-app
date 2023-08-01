8a84ee54c7c59c90f1b77f812944ab549756cefb
Satish-Dabhi <satish@unada.io>
2023-08-01 11:54:40 +0530

    update mongo url

--- server/database/dbConnect.js
+++ server/database/dbConnect.js
@@ -1,11 +1,10 @@
 const mongoose = require("mongoose");
 require("dotenv").config();
 
+const DB_URI = 'mongodb+srv://'+process.env.NAME+':'+process.env.PASSWORD+'@cluster0.djnk3.mongodb.net/recipeApp?retryWrites=true&w=majority';
 
-
-// mongodb://atlas-sql-62359e0dd0f3c742cf1286d5-djnk3.a.query.mongodb.net/myFirstDatabase?ssl=true&authSource=admin
 mongoose
-  .connect(process.env.DB_URL, {
+  .connect(DB_URI, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
   })
