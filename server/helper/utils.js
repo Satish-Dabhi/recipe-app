1659b73184f20958796708f3b12fa19266424e73
Satish-Dabhi <satish@unada.io>
2023-08-02 12:00:26 +0530

    update api key

--- server/helper/utils.js
+++ server/helper/utils.js
@@ -2,8 +2,8 @@ const Constants = {
     RECIPE_SEARCH_API: "https://api.spoonacular.com/recipes/complexSearch",
     RECIPE_DETAIL_API: "https://api.spoonacular.com/recipes/",
     // API_KEY: "2cb9d2b406684c57b1170d778bf3f289"
-    // API_KEY: "f0bf3d1380284f6d8cc2c44e4cec158d"
-    API_KEY: "092b3f310cfa4429922a7b5b55ee0287"
+    API_KEY: "f0bf3d1380284f6d8cc2c44e4cec158d"
+    // API_KEY: "092b3f310cfa4429922a7b5b55ee0287"
 };
 
 module.exports = Constants;
